cdcb67c0cd53a6b8f48c435758a3226d0857e7e5
Dairo Arenas <dairo.arenas802@pascualbravo.edu.co>
2025-07-31 16:52:47 -0500

    Se integra paginador para appointments

--- app/admin/api/appointment/route.ts
+++ app/admin/api/appointment/route.ts
@@ -1,11 +1,16 @@
 import getToken from "@/src/auth/token"
-import { AppointmentListSchema } from "@/src/schemas"
+import { AppointmentAPIResponseSchema } from "@/src/schemas"
 import { notFound } from "next/navigation"
 
 
-export async function GET() {
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+
+    const limit = parseInt(searchParams.get('limit') || '10');
+    const page = parseInt(searchParams.get('page') || '1');
+
     const token = await getToken()
-    const url = `${process.env.API_URL}/appointment/user`
+    const url = `${process.env.API_URL}/appointment/user?limit=${limit}&page=${page}`
     const req = await fetch(url, {
         headers: {
             'Authorization': `Bearer ${token}`
@@ -20,7 +25,7 @@ export async function GET() {
         notFound()
     }
 
-    const appointment = AppointmentListSchema.parse(json)
+    const appointment = AppointmentAPIResponseSchema.parse(json)
 
     return Response.json(appointment)
 }
